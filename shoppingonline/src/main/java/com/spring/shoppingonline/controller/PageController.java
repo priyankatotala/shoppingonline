a4fbf11fdbd149b53b3e48025d1bbe21b6661aed
priyankatotala <39249471+priyankatotala@users.noreply.github.com>
2018-09-20 17:48:03 +1000

    Completed the interface theme and other issues!

--- shoppingonline/src/main/java/com/spring/shoppingonline/controller/PageController.java
+++ shoppingonline/src/main/java/com/spring/shoppingonline/controller/PageController.java
@@ -1,28 +1,21 @@
 package com.spring.shoppingonline.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 	
-	@RequestMapping(value = {"/", "/home", "/index"})
-	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring MVC");
-		return mv;
-	}
+	
 	/*	@RequestMapping(value = {"/test"})
-//  public ModelAndView test(@RequestParam(value="Greeting", required=false)String greeting) {
-//		if(greeting == null) {
-//			greeting = "hello there";
-//		}		
-//		ModelAndView mv = new ModelAndView("page");
-//		mv.addObject("greeting", greeting);
-//		return mv;
+  		public ModelAndView test(@RequestParam(value="Greeting", required=false)String greeting) {
+		if(greeting == null) {
+			greeting = "hello there";
+		}		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
 	
 	}
 	
@@ -36,4 +29,26 @@ public class PageController {
 		return mv;
 	
 	} */
+	
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", "true");
+		return mv;
+	}
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", "true");
+		return mv;
+	}
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", "true");
+		return mv;
+	}
 }
